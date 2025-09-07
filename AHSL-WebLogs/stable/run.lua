5f48f72ecfe9ad923f0a6fd1ce7beeca829e94c8
rtedpro-cpu <admihouse.tip@gmail.com>
2025-09-07 16:57:46 +0200

    Update run.lua

--- AHSL-WebLogs/stable/run.lua
+++ AHSL-WebLogs/stable/run.lua
@@ -1,4 +1,8 @@
 -- CONFIG
-local joinleavelogs = ""
-local chatlogs = ""
+local joinleavelogs = "https://raw.githubusercontent.com/rtedpro-cpu/ahsl-projects/refs/heads/main/AHSL-WebLogs/stable/join-leave-logs.lua"
+local chatlogs = "https://raw.githubusercontent.com/rtedpro-cpu/ahsl-projects/refs/heads/main/AHSL-WebLogs/stable/chat-logs.lua"
+local WebhookUrl = "DISCORD_WEBHOOK_URL_HERE"
+local Webhook = "DISCORD_WEBHOOK_URL_HERE"
 
+loadstring(http.Get(joinleavelogs))
+loadstring(http.Get(chatlogs))
